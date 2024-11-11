d2fa2abb88394a26c3b0744b9e7b023d48079646
YuvanVH <vanessa-huynh@hotmail.com>
2024-11-11 14:49:10 +0100

    added a simple php file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document CMS</title>
+</head>
+
+<body>
+  <h1>PHP here we go</h1>
+</body>
+<br>
+
+<?php
+echo ("im a correct php-file")
+?>
+
+
+</html>
