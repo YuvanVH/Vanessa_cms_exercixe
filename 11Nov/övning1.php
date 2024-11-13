4009381f8b2bdb96b6db7edba233836380b58f52
Vanessa Huynh <127230261+YuvanVH@users.noreply.github.com>
2024-11-13 14:08:14 +0100

    Add files via upload

---
+++ 11Nov/övning1.php
@@ -0,0 +1,10 @@
+<?php
+// Två variabler
+$namn = "Nora";
+$plats = "Stockholm";
+
+// En array med minst tre värden
+$frukter = ["äpple", "banan", "apelsin"];
+
+// Skriv ut en mening som innehåller variablerna och arrayen
+echo "Hej! Jag heter $namn och bor i $plats. Mina favoritfrukter är $frukter[0], $frukter[1] och $frukter[2].";
