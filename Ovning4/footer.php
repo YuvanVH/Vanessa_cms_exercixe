1a9a5ddb0572075f33604ab60a5014ea434d4e77
YuvanVH <vanessa-huynh@hotmail.com>
2024-11-13 15:06:11 +0100

    övning4

---
+++ Ovning4/footer.php
@@ -0,0 +1,7 @@
+<hr>
+<footer>
+  <p>&copy; <?php echo date("Y"); ?> Vanessa Huynh</p>
+</footer>
+</body>
+
+</html>
---
+++ Ovning4/page1.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+
+<h2>Idag</h2>
+
+<?php
+// Visar dagens veckodag och datum
+$locale = 'sv_SE';
+$formatter = new IntlDateFormatter($locale, IntlDateFormatter::FULL, IntlDateFormatter::NONE);
+$formatter->setPattern('EEEE d MMMM'); // Set custom pattern for Swedish date format
+
+$veckodagDatum = $formatter->format(new DateTime());
+echo "<p>Idag är det $veckodagDatum.</p>";
+
+// Kontrollera om datumet är jämnt eller udda
+$dagNummer = date("j");
+if ($dagNummer % 2 === 0) {
+  echo "<p>Den $dagNummer är ett jämnt datum.</p>";
+} else {
+  echo "<p>Den $dagNummer är ett udda datum.</p>";
+}
+?>
+
+<?php include 'footer.php'; ?>
