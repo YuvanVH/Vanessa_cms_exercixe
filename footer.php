c0d92719c5b73ff7b6fad62c5727bb1b8f5db898
YuvanVH <vanessa-huynh@hotmail.com>
2024-11-13 11:49:47 +0100

    lade till övning 4

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Webbplats för Övning4</title>
+</head>
+
+<body>
+  <hr>
+  <footer>
+    <p>&copy; <?php echo date("Y"); ?> (Namn)</p>
+  </footer>
+</body>
+
+</html>
+
+</body>
+
+</html>
