bdef2c932104a806cc651dd05bf88b9ade419cae
YuvanVH <vanessa-huynh@hotmail.com>
2024-11-13 15:40:14 +0100

    Sätter svensk datum format

--- Ovning4/page1.php
+++ Ovning4/page1.php
@@ -3,6 +3,28 @@
 <h2>Idag</h2>
 
 <?php
+
+// allt va inte formaterat på svensk tid :(
+
+// // Hämta dagens veckodag och datum
+// $veckodag = date("l");  // Hämtar veckodagen (måndag, tisdag, etc.)
+// $dag = date("j");  // Hämtar dagens datum (14, 15, etc.)
+// $månad = date("F");  // Hämtar månaden (januari, februari, etc.)
+
+// // Skriv ut veckodagen och datumet
+// echo "<p>Idag är det $veckodag den $dag $månad.</p>";
+
+// // Kontrollera om datumet är jämnt eller udda
+// if ($dag % 2 === 0) {
+//   echo "<p>Den $dag $månad är ett jämnt datum.</p>";
+// } else {
+//   echo "<p>Den $dag $månad är ett udda datum.</p>";
+// }
+//
+?>
+
+
+<?php
 // Visar dagens veckodag & datum
 $locale = 'sv_SE';
 $formatter = new IntlDateFormatter($locale, IntlDateFormatter::FULL, IntlDateFormatter::NONE);
