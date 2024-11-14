e2b9d45dd43cf553427a267cda82c482852beee0
YuvanVH <vanessa-huynh@hotmail.com>
2024-11-14 12:00:23 +0100

    ändrade header titeln

--- Ovning4/header.php
+++ Ovning4/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Webbplats för Övning 4</title>
+  <title>Webbplats för Övning 4 & 5</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
