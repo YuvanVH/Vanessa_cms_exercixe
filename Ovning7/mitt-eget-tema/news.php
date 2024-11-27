907618df0d4751677db750068e31ce6b6e0669e5
YuvanVH <vanessa-huynh@hotmail.com>
2024-11-27 14:06:52 +0100

    precis börjat med news styling men behöver gå vidare

--- Ovning7/mitt-eget-tema/news.php
+++ Ovning7/mitt-eget-tema/news.php
@@ -2,8 +2,19 @@
 include 'includes/header.php';
 ?>
 <main>
-  <h2>Rese- och Kulturnyheter</h2>
-  <p>Få de senaste uppdateringarna om resor, kultur, mat och tips för resenärer!</p>
+  <section class="section">
+    <div class="container">
+      <div class="section-content">
+        <div>
+          <h3>Rese- och Kulturnyheter</h3>
+
+          <hr>
+
+          <p>Få de senaste uppdateringarna om resor, kultur, mat och tips för resenärer!</p>
+        </div>
+      </div>
+    </div>
+  </section>
 
   <div class="news-list">
     <!-- Exempel på nyheter -->
@@ -11,26 +22,32 @@ include 'includes/header.php';
       <section>
         <h3>Semestrar & Resor</h3>
         <article>
-          <img src="images/travel1.jpg" alt="Bild på semesterort" class="news-thumbnail">
-          <h4>10 fantastiska destinationer för 2024</h4>
-          <p>Upptäck årets mest populära resmål för semestern...</p>
-          <a href="single-news.php">Läs mer</a>
+          <div>
+            <img src="images/travel1.jpg" alt="Bild på semesterort" class="news-thumbnail">
+            <h4>10 fantastiska destinationer för 2024</h4>
+            <p>Upptäck årets mest populära resmål för semestern...</p>
+            <a href="single-news.php">Läs mer</a>
+          </div>
         </article>
         <article>
-          <img src="images/travel2.jpg" alt="Vacker strand" class="news-thumbnail">
-          <h4>Reseguide: Asiens bästa stränder</h4>
-          <p>Vill du fly vardagen? Här är några av Asiens bästa stränder...</p>
-          <a href="single-news.php">Läs mer</a>
+          <div>
+            <img src="images/travel2.jpg" alt="Vacker strand" class="news-thumbnail">
+            <h4>Reseguide: Asiens bästa stränder</h4>
+            <p>Vill du fly vardagen? Här är några av Asiens bästa stränder...</p>
+            <a href="single-news.php">Läs mer</a>
+          </div>
         </article>
       </section>
 
       <section>
         <h3>Kultur & Mat</h3>
         <article>
-          <img src="images/food1.jpg" alt="Traditionell maträtt" class="news-thumbnail">
-          <h4>5 rätter du måste prova i Italien</h4>
-          <p>Italien är känt för sin mat. Här är några rätter du inte får missa...</p>
-          <a href="single-news.php">Läs mer</a>
+          <div>
+            <img src="images/food1.jpg" alt="Traditionell maträtt" class="news-thumbnail">
+            <h4>5 rätter du måste prova i Italien</h4>
+            <p>Italien är känt för sin mat. Här är några rätter du inte får missa...</p>
+            <a href="single-news.php">Läs mer</a>
+          </div>
         </article>
       </section>
 
