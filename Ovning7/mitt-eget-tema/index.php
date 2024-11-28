70eb136bad4c61c29b044334d62940962af72f00
YuvanVH <vanessa-huynh@hotmail.com>
2024-11-28 15:28:32 +0100

    grunder till uppgift 8

--- Ovning7/mitt-eget-tema/index.php
+++ Ovning7/mitt-eget-tema/index.php
@@ -1,10 +1,16 @@
 <?php
 include 'includes/header.php';
 ?>
-
-<div>
-  <h1>Världsvyer</h1>
-  <p class="slogan">Varje resa börjar med ett första steg – ta det här.</p>
-</div>
+<section class="section">
+  <div class="container">
+    <div class="section-content">
+      <div>
+        <h3>Världsvyer</h3>
+        <hr>
+        <p class="slogan">Varje resa börjar med ett första steg – ta det här.</p>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php include 'includes/footer.php'; ?>
